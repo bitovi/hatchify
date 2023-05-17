c65dbc22eddf156cf247d1d7a9e0bb168c4b35e9
Arthur Pankiewicz <arthur@bitovi.com>
2023-05-17 14:39:33 -0700

    tranform hatchify-core schema to data-core schema

--- packages/data-core/src/services/types/utils/utils.ts
+++ packages/data-core/src/services/types/utils/utils.ts
@@ -1,6 +1,38 @@
 import type { Schema as OldSchema } from "@hatchifyjs/hatchify-core"
 import type { Schema } from "../types"
 
+// the current backend schema attribute types are sequelize datatypes.
+// this function converts them to the types we use in the frontend.
+// https://sequelize.org/docs/v7/other-topics/other-data-types/
+export function transformDataType(dataType: string): string {
+  const type = dataType.toLowerCase()
+
+  if (
+    type.includes("boolean") ||
+    type.includes("tinyint(1)") ||
+    type.includes("bit")
+  ) {
+    return "boolean"
+  }
+
+  if (
+    type.includes("int") ||
+    type.includes("float") ||
+    type.includes("double") ||
+    type.includes("real") ||
+    type.includes("decimal")
+  ) {
+    return "number"
+  }
+
+  if (type.includes("json")) {
+    return "object"
+  }
+
+  // date, time, now, varchar, ...text, uuid, ...
+  return "string"
+}
+
 export function transformSchema(schema: OldSchema): Schema {
   const resolved: Schema = {
     name: schema.name,
@@ -9,10 +41,15 @@ export function transformSchema(schema: OldSchema): Schema {
   }
 
   for (const [key, value] of Object.entries(schema.attributes)) {
-    resolved.attributes[key] = value
+    resolved.attributes[key] = transformDataType(value)
   }
 
-  if (schema.belongsTo || schema.hasOne || schema.hasMany) {
+  if (
+    schema.belongsTo ||
+    schema.belongsToMany ||
+    schema.hasOne ||
+    schema.hasMany
+  ) {
     resolved.relationships = {}
   }
 
@@ -21,6 +58,15 @@ export function transformSchema(schema: OldSchema): Schema {
       resolved.relationships[belongsTo.options.as] = {
         type: "one",
         schema: belongsTo.target,
+      }
+    }
+  }
+
+  if (schema.belongsToMany && resolved.relationships) {
+    for (const belongsToMany of schema.belongsToMany) {
+      resolved.relationships[belongsToMany.options.as] = {
+        type: "many",
+        schema: belongsToMany.target,
       }
     }
   }
