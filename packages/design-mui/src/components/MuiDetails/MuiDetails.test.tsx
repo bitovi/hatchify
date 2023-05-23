e09939ea3fb08f6e2acabbba9187e9672de71c29
Arthur Pankiewicz <arthur@bitovi.com>
2023-05-23 15:26:34 -0700

    update deps for design-mui

--- packages/design-mui/src/components/MuiDetails/MuiDetails.test.tsx
+++ packages/design-mui/src/components/MuiDetails/MuiDetails.test.tsx
@@ -1,5 +1,5 @@
+import "@testing-library/jest-dom"
 import { describe, it, expect } from "vitest"
-
 import { render, screen } from "@testing-library/react"
 
 import { MuiDetails } from "./MuiDetails"
