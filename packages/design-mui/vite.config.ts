425fc7f4b4f300805b60204ca13ea7087003796d
DanDaManTran <DTran@bitovi.com>
2023-05-23 18:53:25 -0200

    removing data-core for failing CI build

--- packages/design-mui/vite.config.ts
+++ packages/design-mui/vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/react.ts",
+      entry: "src/design-mui.ts",
       formats: ["es", "cjs"],
     },
   },
