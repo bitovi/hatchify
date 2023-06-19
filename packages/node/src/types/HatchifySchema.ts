9fa1ddb41084a172e07265fb0954537b4c021cc7
Roye <roye@bitovi.com>
2023-06-19 11:56:02 -0400

    HATCH-146: BE implement new Schema

---
+++ packages/node/src/types/HatchifySchema.ts
@@ -0,0 +1,112 @@
+type AttributeType = "string" | "number" | "boolean" | "date" | "json"
+
+type RelationshipType = "one" | "many"
+
+export interface HatchifyModelV2 {
+  name: string
+  displayAttribute: string
+  id?: ID
+  attributes: Record<string, Attribute>
+  relationships?: Record<string, Relationship>
+  validation?: Record<string, ModelValidation>
+}
+
+type Attribute =
+  | AttributeType
+  | SimpleAttribute
+  | NonVirtualAttribute
+  | VirtualAttribute
+
+interface SimpleAttribute {
+  type: AttributeType
+  options?: any[]
+}
+
+interface NonVirtualAttribute extends SimpleAttribute {
+  validation?: AttributeValidation
+  sequelize?: AttributeSequelize
+}
+
+interface AttributeSequelize {
+  type: string // "VARCHAR(255)" // TODO: If invalid will throw in runtime?
+}
+
+interface AttributeValidation {
+  optional?: boolean
+  minLength?: number
+}
+
+interface VirtualAttributeBase {
+  virtual: true
+  fields: string[]
+}
+
+type VirtualAttribute =
+  | VirtualAttributeString
+  | VirtualAttributeNumber
+  | VirtualAttributeBoolean
+  | VirtualAttributeDate
+  | VirtualAttributeJson
+
+interface VirtualAttributeString extends VirtualAttributeBase {
+  type: "string"
+  value: (fields: any[]) => string
+}
+
+interface VirtualAttributeNumber extends VirtualAttributeBase {
+  type: "number"
+  value: (fields: any[]) => number
+}
+
+interface VirtualAttributeBoolean extends VirtualAttributeBase {
+  type: "boolean"
+  value: (fields: any[]) => boolean
+}
+
+interface VirtualAttributeDate extends VirtualAttributeBase {
+  type: "date"
+  value: (fields: any[]) => Date
+}
+
+interface VirtualAttributeJson extends VirtualAttributeBase {
+  type: "json"
+  value: (fields: any[]) => object
+}
+
+interface ID {
+  type: string
+  defaultValue?: string
+}
+
+interface Relationship {
+  type: RelationshipType
+  schema: string
+  sequelize?: RelationshipSequelize
+}
+
+interface RelationshipSequelize {
+  through: string
+}
+
+type ModelValidation = SimpleModelValidation | CustomModelValidation
+
+interface SimpleModelValidation {
+  type: "equal" | "less-than"
+  lhs: string
+  rhs: string
+}
+
+type ValidationSuccess = true
+interface ValidationError {
+  fields: string[]
+  message: string
+}
+
+interface CustomModelValidation {
+  type: "custom"
+  fields: string[]
+  validate: (
+    fields: string[],
+    record: Record<string, any>,
+  ) => ValidationSuccess | ValidationError
+}
