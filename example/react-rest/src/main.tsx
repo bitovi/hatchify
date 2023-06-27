745e86edd357b9f7b3317ebad199d68d0961f618
arthurpankiewicz <9858612+arthurpankiewicz@users.noreply.github.com>
2023-06-27 16:26:55 -0700

    increment pre-release versions (#72)
    * increment pre-release versions
    
    * revert example package.json changes

--- example/react-rest/src/main.tsx
+++ example/react-rest/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.js"
-import "./index.css"
 import { worker } from "./mocks/browser"
 
 if (process.env.NODE_ENV === "development") {
