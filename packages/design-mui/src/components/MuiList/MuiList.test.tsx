5537a16e1aa105a0f8ae366019fdefa46e742499
Christina Labay <christina@bitovi.com>
2023-07-05 17:38:39 -0500

    fix build

--- packages/design-mui/src/components/MuiList/MuiList.test.tsx
+++ packages/design-mui/src/components/MuiList/MuiList.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
-import { describe, it, expect } from "vitest"
+import { describe, it, expect, vi } from "vitest"
 import { MuiList } from "./MuiList"
 import {
   HatchifyPresentationDefaultValueComponents,
@@ -57,8 +57,7 @@ describe("hatchifyjs/presentation/mui/MuiList", () => {
             direction: undefined,
             sortBy: false,
           }}
-          changeSort={() => jest.fn()}
-          formatQueryString={() => ""}
+          changeSort={() => vi.fn()}
         />,
       )
 
