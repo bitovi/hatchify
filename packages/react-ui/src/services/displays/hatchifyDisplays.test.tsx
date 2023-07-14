e12e219131da360ac176b7a9209162741889c162
Christina Labay <christina@bitovi.com>
2023-07-14 15:40:02 -0500

    add tests but skip

--- packages/react-ui/src/services/displays/hatchifyDisplays.test.tsx
+++ packages/react-ui/src/services/displays/hatchifyDisplays.test.tsx
@@ -1,13 +1,25 @@
 import "@testing-library/jest-dom"
 import { describe, it, expect, vi } from "vitest"
 import { render, screen } from "@testing-library/react"
-import { getEmptyList } from "./hatchifyDisplays"
+import {
+  getEmptyList,
+  getDisplays,
+  injectExtraDisplays,
+} from "./hatchifyDisplays"
+import type { Schema } from "@hatchifyjs/rest-client"
 import {
   HatchifyList,
   HatchifyEmptyList,
   HatchifyAttributeDisplay,
   HatchifyExtraColumn,
+  HatchifyPresentationDefaultValueComponents,
 } from "../../components"
+
+const TestSchema: Schema = {
+  name: "Test",
+  attributes: { id: "string", name: "string" },
+  displayAttribute: "name",
+}
 
 const testComponents = {
   Test: {
@@ -38,6 +50,94 @@ describe("hatchifyjs/services/hatchifyDisplays", () => {
 
       render(emptyDefault())
       expect(await screen.findByText("So empty inside")).toBeInTheDocument()
+    })
+  })
+
+  //TODO: @arthur plz fix these guys
+  describe.skip("getDisplays", () => {
+    it("works", async () => {
+      const TestList = testComponents.Test.List
+      const TestExtraColumn = testComponents.Test.ExtraColumn
+
+      const displays = getDisplays(
+        TestSchema,
+        undefined,
+        HatchifyPresentationDefaultValueComponents,
+        <TestList>
+          <TestExtraColumn
+            label="Action"
+            render={({ record }) => {
+              return (
+                <>
+                  <button onClick={() => console.log(record)}>Download</button>
+                </>
+              )
+            }}
+          />
+        </TestList>,
+      )
+
+      expect(await displays).toEqual([
+        {
+          key: "id",
+          label: "Id",
+          render: () => vi.fn(),
+        },
+        {
+          key: "name",
+          label: "Name",
+          render: () => vi.fn(),
+        },
+      ])
+    })
+  })
+
+  describe.skip("injectExtraDisplays", () => {
+    it("works", async () => {
+      const TestExtraColumn = testComponents.Test.ExtraColumn
+
+      const displays = injectExtraDisplays(
+        [
+          {
+            key: "id",
+            label: "Id",
+            render: () => vi.fn(),
+          },
+          {
+            key: "name",
+            label: "Name",
+            render: () => vi.fn(),
+          },
+        ],
+        HatchifyPresentationDefaultValueComponents,
+        <TestExtraColumn
+          label="Action"
+          render={({ record }) => {
+            return (
+              <>
+                <button onClick={() => console.log(record)}>Download</button>
+              </>
+            )
+          }}
+        />,
+      )
+      expect(await displays).toEqual([
+        {
+          key: "id",
+          label: "Id",
+          render: () => vi.fn(),
+        },
+        {
+          key: "name",
+          label: "Name",
+          render: () => vi.fn(),
+        },
+        {
+          key: "Action",
+          label: "Action",
+          render: () => vi.fn(),
+        },
+      ])
     })
   })
 })
