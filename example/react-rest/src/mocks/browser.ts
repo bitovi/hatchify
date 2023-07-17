8b77ad4d4994a0f748114b5075a955b7db8018df
arthurpankiewicz <9858612+arthurpankiewicz@users.noreply.github.com>
2023-07-17 08:47:42 -0700

    hatch-153: fixes to react-rest getting started guide (#110)
    * hatch-153: fixes to react-rest getting started guide
    
    * inc versions

--- example/react-rest/src/mocks/browser.ts
+++ example/react-rest/src/mocks/browser.ts
@@ -1,23 +1,50 @@
 // src/mocks/browser.ts
 import { rest, setupWorker } from "msw"
 
-const todos = [
-  { id: "1", type: "Todo", attributes: { name: "Workout" } },
-  { id: "2", type: "Todo", attributes: { name: "Shopping" } },
-  { id: "3", type: "Todo", attributes: { name: "Cooking" } },
+const users = [
+  { id: "1", type: "User", attributes: { name: "John" } },
+  { id: "2", type: "User", attributes: { name: "Jane" } },
+  { id: "3", type: "User", attributes: { name: "Jack" } },
 ]
+
+const todos = [
+  {
+    id: "1",
+    type: "Todo",
+    attributes: { name: "Workout" },
+    relationships: { user: { data: { id: "1", type: "User" } } },
+  },
+  {
+    id: "2",
+    type: "Todo",
+    attributes: { name: "Shopping" },
+    relationships: { user: { data: { id: "2", type: "User" } } },
+  },
+  {
+    id: "3",
+    type: "Todo",
+    attributes: { name: "Cooking" },
+    relationships: { user: { data: { id: "3", type: "User" } } },
+  },
+]
+
+let id = todos.length + 1
 
 const handlers = [
   rest.get("/api/todos", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ data: todos }))
+    return res(ctx.status(200), ctx.json({ data: todos, included: users }))
   }),
   rest.post("/api/todos", async (req, res, ctx) => {
     const json = await req.json()
     const { data } = json
+    const userId = Math.floor(Math.random() * 3) + 1
     const newTodo = {
-      id: (todos.length + 1).toString(),
+      id: (id++).toString(),
       type: "Todo",
       attributes: data.attributes,
+      relationships: {
+        user: { data: { id: userId.toString(), type: "User" } },
+      },
     }
     todos.push(newTodo)
     return res(ctx.status(200), ctx.json({ data: newTodo }))
