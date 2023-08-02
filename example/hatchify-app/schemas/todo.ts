50d1c90c79f5eaa32a2f1e3765f9fa946c06df0a
Christina Labay <christina@bitovi.com>
2023-08-02 16:19:13 -0500

    support nullable (#149)

--- example/hatchify-app/schemas/todo.ts
+++ example/hatchify-app/schemas/todo.ts
@@ -3,9 +3,9 @@
 export const Todo = {
   name: "Todo",
   attributes: {
-    name: "STRING",
-    due_date: "DATE",
-    importance: "INTEGER",
+    name: {type: "STRING", allowNull: true },
+    due_date: {type: "DATE", allowNull: false },
+    importance: {type: "INTEGER", allowNull: true },
   },
   belongsTo: [{ target: "User", options: { as: "user" } }],
 }
