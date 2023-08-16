ab410815dbd5b1e74ec4af4edccc63de6fbc2c04
Nau Nau <nauanycc@outlook.com.br>
2023-08-16 18:19:45 -0300

    HATCH-296: Convert table names to snake_case (#161)
    * feat: ensure schema.name is tested
    
    * refactor: add missing attribute to salesperson
    
    * refactor: move all tests into common file
    
    * feat: fix db name/row casing
    
    * fix /api/api/ in commented tests for future when enabled
    
    * Update guide table name to use snake_case
    
    * remove unwanted function using lodash instead
    
    * Update README.md making through text clearer
    
    ---------
    
    Co-authored-by: abpin <kjoseph.us@gmail.com>
    Co-authored-by: roy-coder <52110946+roy-coder@users.noreply.github.com>
    Co-authored-by: Joseph Khoury <kjoseph7777@gmail.com>

---
+++ packages/koa/src/database.spec.ts
@@ -0,0 +1,82 @@
+import type { HatchifyModel } from "@hatchifyjs/node"
+
+import { startServerWith } from "./testing/utils"
+
+interface Table {
+  tableName: string
+  columns: string[]
+}
+interface TestCase {
+  description: string
+  models: HatchifyModel[]
+  database: Table[]
+}
+
+describe("Naming rules", () => {
+  const databaseNamingTestCases: TestCase[] = [
+    {
+      description: "Ensure database name and columns are in snake case",
+      models: [
+        {
+          name: "SalesPerson",
+          attributes: {
+            name: "STRING",
+            description: "STRING",
+          },
+        },
+        {
+          name: "Account",
+          attributes: {
+            name: "STRING",
+          },
+        },
+        {
+          name: "StateCommercialRepresentative",
+          attributes: {
+            name: "STRING",
+            hasActiveContracts: "STRING",
+          },
+        },
+      ],
+      database: [
+        {
+          tableName: "sales_person",
+          columns: ["id", "name", "description"],
+        },
+        {
+          tableName: "account",
+          columns: ["id", "name"],
+        },
+        {
+          tableName: "state_commercial_representative",
+          columns: ["id", "name", "has_active_contracts"],
+        },
+      ],
+    },
+  ]
+
+  const cases: TestCase[] = [...databaseNamingTestCases]
+
+  let teardown: Awaited<ReturnType<typeof startServerWith>>["teardown"]
+  let hatchify: Awaited<ReturnType<typeof startServerWith>>["hatchify"]
+
+  afterAll(async () => {
+    await teardown()
+  })
+
+  it.each(cases)("$description", async ({ description, models, database }) => {
+    ;({ teardown, hatchify } = await startServerWith(models))
+
+    for (const table of database) {
+      const { columns: expectedColumns, tableName } = table
+
+      const [dbResult] = await hatchify._sequelize.query(
+        `SELECT name FROM pragma_table_info("${tableName}")`,
+      )
+
+      const columns = dbResult.map((row) => row.name)
+
+      expect(columns).toEqual(expect.arrayContaining(expectedColumns))
+    }
+  })
+})
