707c4211980e11ddc9f107aad08055e52d65b864
Nicole Greene <nicolegilbert16@gmail.com>
2023-08-23 10:20:42 -0700

    update

--- client.ts
+++ client.ts
@@ -3,9 +3,9 @@ import { Client } from "pg"
 const pgclient = new Client({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  user: "postgres",
-  password: "postgres",
-  database: "postgres",
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 })
 
 pgclient.connect()
