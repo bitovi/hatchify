d2b980dde96a5e5daed07f8e9b7f946825236dba
roy-coder <52110946+roy-coder@users.noreply.github.com>
2023-08-23 14:50:19 -0400

    HATCH-337: Add datetime v2 (#200)

---
+++ packages/hatchify-core/src/dataTypes/datetime/getPartialControl.ts
@@ -0,0 +1,14 @@
+import type { PartialDatetimeControlType, PartialDatetimeProps } from "./types"
+
+export function getPartialControl(
+  props?: PartialDatetimeProps,
+): PartialDatetimeControlType {
+  return {
+    type: "Datetime",
+    allowNull: props?.required == null ? props?.required : !props.required,
+    min: props?.min,
+    max: props?.max,
+    primary: props?.primary,
+    step: props?.step,
+  }
+}
