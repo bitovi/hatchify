aa0ab122cba14e4152f0e36f2eba4b2c3dfe6bd6
Brad Momberger <brad@bitovi.com>
2023-09-13 17:41:05 -0400

    builder.ts: restore deleted function

--- packages/node/src/parse/builder.ts
+++ packages/node/src/parse/builder.ts
@@ -20,6 +20,80 @@ export interface QueryStringParser<T> {
   orm: "sequelize"
 }
 
+interface Include {
+  association: string
+}
+
+function renameRelationshipFilters(
+  model: HatchifyModel,
+  ops: QueryStringParser<FindOptions>,
+) {
+  const errors: Error[] = []
+
+  function _renameRelationshipFilters(where) {
+    if (typeof where !== "object") {
+      return where
+    }
+
+    if (Array.isArray(where)) {
+      return where.map(_renameRelationshipFilters)
+    }
+
+    return [
+      ...Object.getOwnPropertyNames(where),
+      ...Object.getOwnPropertySymbols(where),
+    ].reduce((acc, key) => {
+      if (typeof key === "symbol") {
+        return { ...acc, [key]: _renameRelationshipFilters(where[key]) }
+      }
+
+      if (!key.includes(".")) {
+        if (key !== "id" && !model.attributes[key]) {
+          errors.push(
+            new UnexpectedValueError({
+              detail: `URL must have 'filter[x]' where 'x' is one of ${Object.keys(
+                model.attributes,
+              )
+                .map((attribute) => `'${attribute}'`)
+                .join(", ")}.`,
+              parameter: `filter[${key}]`,
+            }),
+          )
+        }
+
+        return {
+          ...acc,
+          [key]: _renameRelationshipFilters(where[key]),
+        }
+      }
+
+      const [relationshipName] = key.split(".")
+
+      const relationshipNames = (
+        !ops.data.include || Array.isArray(ops.data.include)
+          ? ((ops.data.include ?? []) as Include[])
+          : [ops.data.include as Include]
+      ).map((include) => include.association)
+
+      if (!relationshipNames.includes(relationshipName)) {
+        errors.push(
+          new UnexpectedValueError({
+            detail: `URL must have 'filter[${key}]' where '${relationshipName}' is one of the includes.`,
+            parameter: `filter[${key}]`,
+          }),
+        )
+      }
+
+      return {
+        ...acc,
+        [`$${key}$`]: _renameRelationshipFilters(where[key]),
+      }
+    }, {})
+  }
+
+  return { where: _renameRelationshipFilters(ops.data.where), errors }
+}
+
 export function buildFindOptions(
   hatchify: Hatchify,
   model: HatchifyModel,
