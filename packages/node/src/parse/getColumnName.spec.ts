f1153d6034fc87d0fdff4a7e41d9540422955aa0
Roye <roye@bitovi.com>
2023-09-14 19:04:00 -0400

    cleanup

--- packages/node/src/parse/getColumnName.spec.ts
+++ packages/node/src/parse/getColumnName.spec.ts
@@ -2,14 +2,14 @@ import { getColumnName } from "./getColumnName"
 
 describe("getColumnName", () => {
   it("gets the correct column word for SQLite", () => {
-    expect(getColumnName("word", "sqlite")).toBe("`word`")
-    expect(getColumnName("twoWords", "sqlite")).toBe("`two_words`")
+    expect(getColumnName("word", "sqlite")).toBe("word")
+    expect(getColumnName("twoWords", "sqlite")).toBe("two_words")
     expect(getColumnName("itemA.itemB", "sqlite")).toBe("`itemA.item_b`")
   })
 
   it("gets the correct column word for Postgres", () => {
-    expect(getColumnName("word", "postgres")).toBe(`"word"`)
-    expect(getColumnName("twoWords", "postgres")).toBe(`"two_words"`)
+    expect(getColumnName("word", "postgres")).toBe("word")
+    expect(getColumnName("twoWords", "postgres")).toBe("two_words")
     expect(getColumnName("itemA.itemB", "postgres")).toBe(`"itemA.item_b"`)
   })
 })
--- packages/node/src/parse/getColumnName.ts
+++ packages/node/src/parse/getColumnName.ts
@@ -2,6 +2,10 @@ import { noCase } from "no-case"
 import type { Dialect } from "sequelize"
 
 export function getColumnName(attributeName: string, dialect: Dialect): string {
+  if (!attributeName.includes(".")) {
+    return noCase(attributeName, { delimiter: "_" })
+  }
+
   const columnName = attributeName
     .replaceAll("$", "")
     .split(".")
