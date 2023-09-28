c3e71b70ba790dffe9ce93b277918bc3ee218ed6
Arthur Pankiewicz <arthur@bitovi.com>
2023-09-28 10:39:15 -0700

    fix: crypto global for browser in uuidv4

--- packages/hatchify-core/src/util/uuidv4.ts
+++ packages/hatchify-core/src/util/uuidv4.ts
@@ -1,9 +1,15 @@
-import { getRandomValues } from "crypto"
+function getCrypto() {
+  return typeof process === "object" &&
+    {}.toString.call(process) === "[object process]"
+    ? require("crypto")
+    : window.crypto
+}
 
 export function uuidv4(): string {
   return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
-    (+c ^ (getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(
-      16,
-    ),
+    (
+      +c ^
+      (getCrypto().getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
+    ).toString(16),
   )
 }
