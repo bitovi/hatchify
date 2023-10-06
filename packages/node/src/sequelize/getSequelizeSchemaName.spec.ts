da12b3cac64916b7956cf7a90b6f17e4e6c70de8
Roye <roye@bitovi.com>
2023-10-06 10:12:52 -0400

    test

--- packages/node/src/sequelize/getSequelizeSchemaName.spec.ts
+++ packages/node/src/sequelize/getSequelizeSchemaName.spec.ts
@@ -2,7 +2,7 @@ import { getSequelizeSchemaName } from "./getSequelizeSchemaName"
 
 describe("getSequelizeSchemaName", () => {
   it("is always undefined for sqlite", () => {
-    expect(getSequelizeSchemaName("sqlite", "schema")).toBeUndefined()
+    expect(getSequelizeSchemaName("sqlite", "schemaName")).toBeUndefined()
     expect(getSequelizeSchemaName("sqlite", "")).toBeUndefined()
     expect(getSequelizeSchemaName("sqlite", undefined)).toBeUndefined()
     expect(
@@ -11,7 +11,7 @@ describe("getSequelizeSchemaName", () => {
   })
 
   it("is always defined for postgres", () => {
-    expect(getSequelizeSchemaName("postgres", "schema")).toBe("schema")
+    expect(getSequelizeSchemaName("postgres", "schemaName")).toBe("schema_name")
     expect(getSequelizeSchemaName("postgres", "")).toBe("public")
     expect(getSequelizeSchemaName("postgres", undefined)).toBe("public")
     expect(getSequelizeSchemaName("postgres", null as unknown as string)).toBe(
