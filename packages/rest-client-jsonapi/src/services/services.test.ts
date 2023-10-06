3fd82daef087dd70ec42e3913459a4d7e27425eb
Luca <60432429+luca-bitovi@users.noreply.github.com>
2023-10-06 10:03:43 -0400

    Adding E2E tests

--- packages/rest-client-jsonapi/src/services/services.test.ts
+++ packages/rest-client-jsonapi/src/services/services.test.ts
@@ -3,11 +3,22 @@ import Koa from "koa"
 import { describe, expect, it } from "vitest"
 import { hatchifyKoa } from "@hatchifyjs/koa"
 import hatchifyReactRest from "@hatchifyjs/react-rest"
+import type { Schema } from "@hatchifyjs/react-rest"
 import jsonapi from "../rest-client-jsonapi"
 import { testBackendEndpointConfig } from "../setupTests"
 
-const Article = {
+const Article: Schema = {
   name: "Article",
+  displayAttribute: "name",
+  attributes: {
+    author: { type: "STRING", allowNull: false },
+    tag: { type: "STRING", allowNull: false },
+  },
+}
+
+const Feature_Article: Schema = {
+  name: "Article",
+  namespace: "Feature",
   displayAttribute: "name",
   attributes: {
     author: { type: "STRING", allowNull: false },
@@ -72,6 +83,68 @@ describe("Testing CRUD operations against Hatchify backend", async () => {
 
     await hatchedReactRest.Article.deleteOne(id)
     await expect(() => hatchedReactRest.Article.findOne(id)).rejects.toThrow()
+    expect.assertions(4) // Useful for confirming that assertions were actually called against asynchronous functions
+  })
+
+  it("successfully runs CRUD operations on schemas with namespaces", async () => {
+    const app = new Koa()
+    const hatchedKoa = hatchifyKoa([Feature_Article], {
+      prefix: `/${testBackendEndpointConfig.api}`,
+      database: {
+        dialect: "sqlite",
+        storage: ":memory:",
+      },
+    })
+    app.use(cors())
+    app.use(hatchedKoa.middleware.allModels.all)
+    await hatchedKoa.createDatabase()
+    app.listen(3001)
+
+    const jsonApi = jsonapi(
+      `http://localhost:3001/${testBackendEndpointConfig.api}`,
+      {
+        Feature_Article: { endpoint: `${testBackendEndpointConfig.schema}` },
+      },
+    )
+    const hatchedReactRest = hatchifyReactRest({ Feature_Article }, jsonApi)
+
+    await hatchedReactRest.Feature_Article.createOne({
+      attributes: {
+        author: "John Doe",
+        tag: "Hatchify",
+      },
+    })
+    const [featureArticles] = await hatchedReactRest.Feature_Article.findAll({})
+    const [featureArticle] = featureArticles
+    const { id } = featureArticle
+    expect(featureArticles.length === 1)
+    expect(featureArticle).toEqual({
+      id,
+      __schema: "Feature_Article",
+      author: "John Doe",
+      tag: "Hatchify",
+    })
+
+    await hatchedReactRest.Feature_Article.updateOne({
+      id,
+      attributes: {
+        author: "John Doe Updated",
+        tag: "Hatchify Updated",
+      },
+    })
+    const updatedFeatureArticleQuery =
+      await hatchedReactRest.Feature_Article.findOne(id)
+    expect(updatedFeatureArticleQuery).toEqual({
+      id,
+      __schema: "Feature_Article",
+      author: "John Doe Updated",
+      tag: "Hatchify Updated",
+    })
+
+    await hatchedReactRest.Feature_Article.deleteOne(id)
+    await expect(() =>
+      hatchedReactRest.Feature_Article.findOne(id),
+    ).rejects.toThrow()
     expect.assertions(4) // Useful for confirming that assertions were actually called against asynchronous functions
   })
 })
