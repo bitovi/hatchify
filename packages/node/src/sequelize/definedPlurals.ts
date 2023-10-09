9d06b9cf46a604446157e87c55b1b67e6758e4e0
roy-coder <52110946+roy-coder@users.noreply.github.com>
2023-10-09 13:19:14 -0400

    Fix pluralName (#274)

--- packages/node/src/sequelize/definedPlurals.ts
+++ packages/node/src/sequelize/definedPlurals.ts
@@ -1,11 +1,14 @@
 import type { HatchifyModel } from "../types"
+import { getFullModelName } from "../utils/getFullModelName"
 
 export function definedPlurals(
   models: HatchifyModel[],
 ): Record<string, string> {
   return models.reduce(
-    (acc, { name, pluralName }) =>
-      pluralName ? { ...acc, [name]: pluralName } : acc,
+    (acc, model) =>
+      model.pluralName
+        ? { ...acc, [getFullModelName(model)]: model.pluralName }
+        : acc,
     {},
   )
 }
