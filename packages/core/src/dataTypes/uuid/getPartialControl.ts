accecd5acd804eb8a6bd2a56583d911352212b86
Arthur Pankiewicz <arthur@bitovi.com>
2023-10-13 17:49:53 -0700

    add generic for uuid

--- packages/core/src/dataTypes/uuid/getPartialControl.ts
+++ packages/core/src/dataTypes/uuid/getPartialControl.ts
@@ -2,15 +2,15 @@ import { UUID_REGEX } from "./constants"
 import type { PartialUuidProps } from "./types"
 import type { PartialStringControlType } from "../string"
 
-export function getPartialControl(
-  props?: PartialUuidProps,
-): PartialStringControlType<boolean> {
+export function getPartialControl<TRequired extends boolean>(
+  props?: PartialUuidProps<TRequired>,
+): PartialStringControlType<TRequired> {
   return {
     type: "String",
     allowNull: props?.required == null ? props?.required : !props.required,
     allowNullInfer: (props?.required == null
       ? props?.required
-      : !props.required) as boolean,
+      : !props.required) as TRequired extends true ? false : true,
     min: 36,
     max: 36,
     primary: props?.primary,
