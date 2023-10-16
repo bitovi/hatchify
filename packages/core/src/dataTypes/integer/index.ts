e40aec5b9f320ff6dc253a80bb258e20dba89b40
Arthur Pankiewicz <arthur@bitovi.com>
2023-10-16 13:28:11 -0700

    remove testing comment

--- packages/core/src/dataTypes/integer/index.ts
+++ packages/core/src/dataTypes/integer/index.ts
@@ -26,26 +26,3 @@ export function integer<TRequired extends boolean = false>(
     },
   }
 }
-
-// type Prettify<T> = {
-//   [K in keyof T]: T[K]
-// } & {}
-
-// type B1 = Prettify<PartialNumberControlType<false>>["allowNullInfer"]
-// //   ^?
-// type BB1 = Prettify<PartialNumberControlType<true>>["allowNullInfer"]
-// //   ^?
-// type B2 = Prettify<PartialNumberControlType<false>>["allowNull"]
-// //   ^?
-// type BB2 = Prettify<PartialNumberControlType<true>>["allowNull"]
-// //   ^?
-// const aaaa1 = integer({ required: true })
-// type A1 = Prettify<typeof aaaa1>["control"]["allowNullInfer"]
-// //   ^?
-// type AA1 = Prettify<typeof aaaa1>["control"]["allowNull"]
-// //   ^?
-// const aaaa2 = integer()
-// type A2 = Prettify<typeof aaaa2>["control"]["allowNullInfer"]
-// //   ^?
-// type AA2 = Prettify<typeof aaaa2>["control"]["allowNull"]
-// //   ^?
