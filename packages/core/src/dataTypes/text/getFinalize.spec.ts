56d140e0f06a7d2ca2964eff13208d20269cdad8
Christina Labay <christina@bitovi.com>
2023-10-25 11:09:35 -0500

    Add core functions

---
+++ packages/core/src/dataTypes/text/getFinalize.spec.ts
@@ -0,0 +1,25 @@
+import { text } from "."
+
+describe("getFinalize", () => {
+  it("finalizes a partial attribute", () => {
+    expect(text()).toEqual({
+      name: "text()",
+      control: {
+        allowNull: undefined,
+        max: Infinity,
+        min: 0,
+        primary: undefined,
+        regex: undefined,
+        type: "String",
+      },
+      orm: {
+        sequelize: {
+          allowNull: undefined,
+          primaryKey: undefined,
+          type: "TEXT",
+        },
+      },
+      finalize: expect.any(Function),
+    })
+  })
+})
