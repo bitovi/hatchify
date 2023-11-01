11ae6868a1fe115b2b379a26f625d406de15b98e
Christina Labay <christina@bitovi.com>
2023-11-01 16:53:12 -0500

    Hatch 349/enums - fix control type (#304)

--- packages/core/src/dataTypes/enumerate/coerce.spec.ts
+++ packages/core/src/dataTypes/enumerate/coerce.spec.ts
@@ -5,23 +5,23 @@ describe("coerce", () => {
   const values = ["foo", "bar"]
 
   it("handles undefined", () => {
-    expect(() => coerce(undefined, { type: "String", values })).toThrow(
+    expect(() => coerce(undefined, { type: "enum", values })).toThrow(
       new HatchifyCoerceError("as a non-undefined value"),
     )
   })
 
   it("handles null", () => {
-    expect(coerce(null, { type: "String", allowNull: true, values })).toBeNull()
+    expect(coerce(null, { type: "enum", allowNull: true, values })).toBeNull()
     expect(() =>
-      coerce(null, { type: "String", allowNull: false, values }),
+      coerce(null, { type: "enum", allowNull: false, values }),
     ).toThrow(new HatchifyCoerceError("as a non-null value"))
   })
 
   it("handles non-strings", () => {
-    expect(() => coerce(1, { type: "String", values })).toThrow(
+    expect(() => coerce(1, { type: "enum", values })).toThrow(
       new HatchifyCoerceError("as a string"),
     )
-    expect(() => coerce({}, { type: "String", values })).toThrow(
+    expect(() => coerce({}, { type: "enum", values })).toThrow(
       new HatchifyCoerceError("as a string"),
     )
     expect(() =>
@@ -29,13 +29,13 @@ describe("coerce", () => {
         () => {
           /* noop */
         },
-        { type: "String", values },
+        { type: "enum", values },
       ),
     ).toThrow(new HatchifyCoerceError("as a string"))
   })
 
   it("handles values", () => {
-    expect(() => coerce("baz", { type: "String", values })).toThrow(
+    expect(() => coerce("baz", { type: "enum", values })).toThrow(
       new HatchifyCoerceError("as one of 'foo', 'bar'"),
     )
   })
