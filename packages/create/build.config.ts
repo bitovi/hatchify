eba639baa5aa2190718d7d713c02bed9d5f679d2
Roye <roye@bitovi.com>
2023-11-07 16:53:19 -0500

    try

--- packages/create/build.config.ts
+++ packages/create/build.config.ts
@@ -1,4 +1,8 @@
+import path from "node:path"
+import url from "node:url"
 import { defineBuildConfig } from "unbuild"
+
+path.dirname(url.fileURLToPath(import.meta.url))
 
 export default defineBuildConfig({
   entries: ["src/index"],
