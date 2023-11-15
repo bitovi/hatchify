0692d69f4c5fdf72cde76db817e8fa43dd92a10b
Arthur Pankiewicz <arthur@bitovi.com>
2023-11-15 15:39:51 -0800

    update getEmptyList fn

--- packages/react-ui/src/hooks/useCompoundComponents/helpers/getEmptyList.tsx
+++ packages/react-ui/src/hooks/useCompoundComponents/helpers/getEmptyList.tsx
@@ -1,5 +1,7 @@
 export function getEmptyList(childArray: JSX.Element[]): () => JSX.Element {
-  const emptyComponent = childArray.find((c) => c.type.name === "Empty")
+  const emptyComponent = childArray.find(
+    (c) => c.type.name === "Empty" || c.type.displayName === "Empty",
+  )
   const emptyDisplay: JSX.Element = emptyComponent?.props.children || undefined
 
   const EmptyList = () => emptyDisplay || <div>No records found</div>
