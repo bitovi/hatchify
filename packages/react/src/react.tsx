41b3d146b9374a5d7835979a366d52080b4d1a55
Arthur Pankiewicz <arthur@bitovi.com>
2023-11-16 12:18:08 -0800

    remove core v1 schema code

--- packages/react/src/react.tsx
+++ packages/react/src/react.tsx
@@ -1,4 +1,4 @@
-export type { Schema, PartialSchema } from "@hatchifyjs/core"
+export type { PartialSchema } from "@hatchifyjs/core"
 export { default as hatchifyReact } from "@hatchifyjs/react-ui"
 export type { HatchifyApp, CollectionState } from "@hatchifyjs/react-ui"
 export {
