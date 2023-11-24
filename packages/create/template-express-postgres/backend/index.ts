822b1bb89bbdeaa9589dcf0e489907f4999213c3
Nils Lundquist <stealthwang@gmail.com>
2023-11-24 16:20:49 -0700

    HATCH-437: update templates to use integrated dev server process (#338)
    * update templates to use integrated dev server process
    
    ---------
    
    Co-authored-by: Nils Lundquist <nils@bitovi.com>

--- packages/create/template-express-postgres/backend/index.ts
+++ packages/create/template-express-postgres/backend/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import Express from "express"
-import cors from "cors"
+import { createServer as createViteServer } from "vite"
 import { hatchifyExpress } from "@hatchifyjs/express"
 import * as Schemas from "../schemas"
 
@@ -19,10 +19,16 @@ const hatchedExpress = hatchifyExpress(Schemas, {
   },
 })
 
-app.use(cors())
-app.use(hatchedExpress.middleware.allModels.all)
 ;(async () => {
   await hatchedExpress.createDatabase()
+
+  const vite = await createViteServer({
+    root: `${__dirname}/../`,
+    server: { middlewareMode: true },
+  })
+
+  app.use(hatchedExpress.middleware.allModels.all)
+  app.use(vite.middlewares)
 
   app.listen(3000, () => {
     console.log("Started on port 3000")
