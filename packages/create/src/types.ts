3bb167f3dea8ea92a70cd0465f0730bf010ac2ea
Roye <roye@bitovi.com>
2023-11-24 12:46:32 -0500

    Cleanup create

--- packages/create/src/types.ts
+++ packages/create/src/types.ts
@@ -1,13 +1,22 @@
 type ColorFunc = (str: string | number) => string
 
-export type Framework = {
+export type Backend = {
   name: string
   display: string
   color: ColorFunc
   dependencies: string[]
   devDependencies: string[]
 }
-export type Dialect = {
+
+export type Database = {
+  name: string
+  display: string
+  color: ColorFunc
+  dependencies: string[]
+  devDependencies: string[]
+}
+
+export type Frontend = {
   name: string
   display: string
   color: ColorFunc
