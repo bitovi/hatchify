5148dad3c863ea4240d24b4d99abe542009f06be
Roye <roye@bitovi.com>
2023-12-08 09:20:39 -0500

    fix: example/react-rest-hatchify-backend was using cors

--- example/react-rest-hatchify-backend/backend/index.ts
+++ example/react-rest-hatchify-backend/backend/index.ts
@@ -1,6 +1,8 @@
 // hatchify-app/backend/index.ts
 import Koa from "koa"
-import cors from "@koa/cors"
+import c2k from "koa-connect"
+// @ts-expect-error @todo make TS happy here
+import { createServer as createViteServer } from "vite.mts"
 import { hatchifyKoa } from "@hatchifyjs/koa"
 import { Todo } from "../schemas/todo"
 import { User } from "../schemas/user"
@@ -8,10 +10,16 @@ import { User } from "../schemas/user"
 const app = new Koa()
 const hatchedKoa = hatchifyKoa({ Todo, User }, { prefix: "/api" })
 
-app.use(cors())
-app.use(hatchedKoa.middleware.allModels.all)
 ;(async () => {
   await hatchedKoa.modelSync({ alter: true })
+
+  const vite = await createViteServer({
+    root: `${__dirname}/../`,
+    server: { middlewareMode: true },
+  })
+
+  app.use(hatchedKoa.middleware.allModels.all)
+  app.use(c2k(vite.middlewares))
 
   app.listen(3000, () => {
     console.log("Started on port 3000")
