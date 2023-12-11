bc848560ce2091b175cbf8d0df485dbc1a7b2cfc
roy-coder <52110946+roy-coder@users.noreply.github.com>
2023-12-11 16:21:19 -0500

    HATCH-428: Filtering on UUID (#370)

---
+++ packages/node/src/parse/handlePostgresUuid.ts
@@ -0,0 +1,73 @@
+import { isISO8601DateString, isISO8601DatetimeString } from "@hatchifyjs/core"
+import type { Dialect, FindOptions } from "sequelize"
+import { Op, Sequelize } from "sequelize"
+
+import type { QueryStringParser } from "./builder"
+import { getColumnName } from "./getColumnName"
+import { walk } from "./walk"
+
+function isString(string: string): boolean {
+  return (
+    typeof string === "string" &&
+    !isISO8601DatetimeString(string) &&
+    !isISO8601DateString(string)
+  )
+}
+
+function parse(value: unknown): {
+  operator?: typeof Op.eq | typeof Op.iLike | typeof Op.like
+  innerValue?: string
+} {
+  if (isString((value as { [Op.eq]: any })[Op.eq])) {
+    return {
+      operator: Op.eq,
+      innerValue: (value as { [Op.eq]: any })[Op.eq],
+    }
+  }
+
+  if (isString((value as { [Op.iLike]: any })[Op.iLike])) {
+    return {
+      operator: Op.iLike,
+      innerValue: (value as { [Op.iLike]: any })[Op.iLike],
+    }
+  }
+
+  if (isString((value as { [Op.like]: any })[Op.like])) {
+    return {
+      operator: Op.like,
+      innerValue: (value as { [Op.like]: any })[Op.like],
+    }
+  }
+
+  return {}
+}
+
+export function handlePostgresUuid(
+  ops: QueryStringParser<FindOptions>,
+  dialect: Dialect,
+): QueryStringParser<FindOptions> {
+  if (dialect === "postgres") {
+    ops.data.where = walk<typeof ops.data.where>(
+      ops.data.where,
+      (key, value) => {
+        if (typeof key !== "string") {
+          return
+        }
+
+        const { operator, innerValue } = parse(value)
+
+        if (!operator) {
+          return
+        }
+
+        const token = Sequelize.where(
+          Sequelize.cast(Sequelize.col(getColumnName(key, dialect)), "varchar"),
+          operator,
+          innerValue,
+        )
+        return [[token], Op.and]
+      },
+    )
+  }
+  return ops
+}
