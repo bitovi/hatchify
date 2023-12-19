9999a869b40dec4466a3daaff45e40d42b823d71
Nils Lundquist <stealthwang@gmail.com>
2023-12-19 13:08:05 -0700

    fix hatchifyjs/react type export  (#382)
    * fix hatchifyjs/react type export and broke pree2e.sh into it's own shell script
    
    * remove npx usage
    
    ---------
    
    Co-authored-by: Nils Lundquist <nils@bitovi.com>

---
+++ scripts/pree2e.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd example;
+rm -rf hatchify-app;
+node ../packages/create/index.js hatchify-app \
+    --frontend ${npm_config_frontend} \
+    --backend ${npm_config_backend} \
+    --database ${npm_config_database} \
+    --path ../../packages;
+cp schemas.txt hatchify-app/schemas.ts;
+cp App.txt hatchify-app/frontend/App.tsx;
