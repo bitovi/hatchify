823bb0c1499b4ae3211c075f4d3ed972ee02f561
Noah Turner <aminnoah19@gmail.com>
2023-12-28 16:52:38 -0600

    added casing test

--- packages/core/src/util/camelCaseToTitleCase.spec.ts
+++ packages/core/src/util/camelCaseToTitleCase.spec.ts
@@ -4,8 +4,9 @@ describe("camelCaseToTitleCase", () => {
   it("works as expected", () => {
     expect(camelCaseToTitleCase("user")).toBe("User")
     expect(camelCaseToTitleCase("userName")).toBe("User Name")
-    expect(camelCaseToTitleCase("parent12LastName")).toBe("Parent 12 Last Name")
-    expect(camelCaseToTitleCase("addressLine12345")).toBe("Address Line 12345")
+    expect(camelCaseToTitleCase("parent1LastName")).toBe("Parent 1 Last Name")
+    expect(camelCaseToTitleCase("addressLine1")).toBe("Address Line 1")
+    expect(camelCaseToTitleCase("zipCode12345")).toBe("Zip Code 12345")
     expect(camelCaseToTitleCase("u")).toBe("U")
     expect(camelCaseToTitleCase("")).toBe("")
     expect(camelCaseToTitleCase(null as unknown as string)).toBeNull()
