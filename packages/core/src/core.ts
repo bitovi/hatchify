3c740227ccf2be0cb6c57d484bd91968494b4d0c
Arthur Pankiewicz <arthur@bitovi.com>
2024-01-22 10:19:29 -0800

    fix: explicit type export for type inference with esm

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -11,3 +11,71 @@ export * from "./util/pascalCaseToKebabCase.js"
 export * from "./util/pluralize.js"
 export * from "./util/singularize.js"
 export * from "./util/uuidv4.js"
+
+// required to explicitly export types for inference with esm modules!!
+export type {
+  FinalBooleanORM,
+  PartialBooleanControlType,
+  PartialBooleanORM,
+  PartialBooleanProps,
+} from "./dataTypes/boolean/types.js"
+export type {
+  FinalDateonlyORM,
+  PartialDateonlyControlType,
+  PartialDateonlyORM,
+  PartialDateonlyProps,
+} from "./dataTypes/dateonly/types.js"
+export type {
+  FinalDatetimeORM,
+  PartialDatetimeControlType,
+  PartialDatetimeORM,
+  PartialDatetimeProps,
+} from "./dataTypes/datetime/types.js"
+export type {
+  FinalEnumORM,
+  PartialEnumControlType,
+  PartialEnumORM,
+  PartialEnumProps,
+} from "./dataTypes/enumerate/types.js"
+export type { PartialIntegerProps } from "./dataTypes/integer/types.js"
+export type {
+  FinalNumberORM,
+  PartialNumberControlType,
+  PartialNumberORM,
+  PartialNumberProps,
+} from "./dataTypes/number/types.js"
+export type {
+  FinalStringORM,
+  PartialStringControlType,
+  PartialStringORM,
+  PartialStringProps,
+} from "./dataTypes/string/types.js"
+export type {
+  FinalTextORM,
+  PartialTextControlType,
+  PartialTextORM,
+  PartialTextProps,
+} from "./dataTypes/text/types.js"
+export type {
+  FinalUuidORM,
+  PartialUuidControlType,
+  PartialUuidORM,
+  PartialUuidProps,
+} from "./dataTypes/uuid/types.js"
+export type {
+  PartialBelongsToRelationship,
+  FinalBelongsToRelationship,
+} from "./relationships/belongsTo/types.js"
+export type {
+  PartialHasManyRelationship,
+  FinalHasManyRelationship,
+} from "./relationships/hasMany/types.js"
+export type {
+  PartialHasManyThroughRelationship,
+  FinalHasManyThroughRelationship,
+  ThroughOptions,
+} from "./relationships/hasManyThrough/types.js"
+export type {
+  PartialHasOneRelationship,
+  FinalHasOneRelationship,
+} from "./relationships/hasOne/types.js"
