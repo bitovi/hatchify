fe250036ab7f52ef571a62afb803b015a9918ad5
Arthur Pankiewicz <arthur@bitovi.com>
2024-01-24 14:41:49 -0800

    tsc output cjs

--- packages/core/src/util/uuidv4.ts
+++ packages/core/src/util/uuidv4.ts
@@ -1,10 +1,16 @@
-/* istanbul ignore next */
-const crypto = globalThis.crypto || (await import("node:crypto")).webcrypto
-
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function getCrypto() {
-  return crypto
+  return typeof window === "undefined"
+    ? // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require("node:crypto").webcrypto
+    : window.crypto
 }
 
 export function uuidv4(): string {
-  return crypto.randomUUID()
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+    (
+      +c ^
+      (getCrypto().getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
+    ).toString(16),
+  )
 }
