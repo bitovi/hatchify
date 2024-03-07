5239c4fc6a500ea1c25463ecbce27fffc50f4ead
Arthur Pankiewicz <arthur@bitovi.com>
2024-03-07 16:09:50 -0600

    remove unused import

--- packages/design-mui/src/components/MuiProvider/MuiProvider.test.tsx
+++ packages/design-mui/src/components/MuiProvider/MuiProvider.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest"
+import { describe, it } from "vitest"
 import { render, screen } from "@testing-library/react"
 import { MuiProvider } from "."
 import hatchifyReact from "@hatchifyjs/react-ui"
