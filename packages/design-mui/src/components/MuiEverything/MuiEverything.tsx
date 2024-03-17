286228760dd18ada9d65cf3a7fbb757df6cbf4a6
Justin Meyer <justin@bitovi.com>
2024-03-17 14:54:06 -0500

    removing imports outside of @mui/material (#514)
    * removing imports outside of @mui/material
    
    * fixing linting error
    
    * another prettier problem

--- packages/design-mui/src/components/MuiEverything/MuiEverything.tsx
+++ packages/design-mui/src/components/MuiEverything/MuiEverything.tsx
@@ -1,7 +1,4 @@
-import Grid from "@mui/material/Grid/index.js"
-import Tabs from "@mui/material/Tabs/index.js"
-import Tab from "@mui/material/Tab/index.js"
-import Typography from "@mui/material/Typography/index.js"
+import { Grid, Tabs, Tab, Typography } from "@mui/material"
 import { HatchifyEmpty } from "@hatchifyjs/react-ui"
 import type { XEverythingProps } from "@hatchifyjs/react-ui"
 import type { PartialSchema } from "@hatchifyjs/core"
