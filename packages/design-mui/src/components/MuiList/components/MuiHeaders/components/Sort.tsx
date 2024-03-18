7de37220d2fde6fa6f2a874c2d189ff9dd3fab00
Roye <roye@bitovi.com>
2024-03-18 15:45:29 -0400

    revert

--- packages/design-mui/src/components/MuiList/components/MuiHeaders/components/Sort.tsx
+++ packages/design-mui/src/components/MuiList/components/MuiHeaders/components/Sort.tsx
@@ -1,6 +1,7 @@
 import type { HatchifyColumn, HeaderProps } from "@hatchifyjs/react-ui"
 import type { Meta } from "@hatchifyjs/rest-client"
 import { Box, TableSortLabel } from "@mui/material"
+import { visuallyHidden } from "@mui/utils"
 
 export const Sort: React.FC<
   Pick<HeaderProps, "direction" | "setSort" | "sortBy"> & {
@@ -20,7 +21,7 @@ export const Sort: React.FC<
   >
     {children}
     {columnKey === sortBy ? (
-      <Box component="span">
+      <Box component="span" sx={visuallyHidden}>
         {direction === "desc" ? "sorted descending" : "sorted ascending"}
       </Box>
     ) : null}
