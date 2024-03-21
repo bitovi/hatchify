543de47e04373aa2b862a0b2e43d2f9fdc771f9c
Roye <roye@bitovi.com>
2024-03-21 13:28:47 -0400

    lint

--- packages/design-mui/src/components/MuiFilters/components/inputs/ValueInput.tsx
+++ packages/design-mui/src/components/MuiFilters/components/inputs/ValueInput.tsx
@@ -1,10 +1,10 @@
+import type { StringStep } from "@hatchifyjs/core"
 import { InputLabel } from "@mui/material"
 import type { FilterableControls, Operators } from "../../constants.js"
 import DateInput from "./DateInput.js"
 import EnumInput from "./EnumInput.js"
 import StringInput from "./StringInput.js"
 import NumberInput from "./NumberInput.js"
-import type { StringStep } from "packages/core/dist/core.js"
 
 const ValueInput: React.FC<{
   labelId: string
