dbecf6bd8fd8a55ef28522ed3500d42904d48f30
Roye <roye@bitovi.com>
2024-03-21 13:31:09 -0400

    cleanup

--- packages/design-mui/src/components/MuiFilters/components/inputs/DateInput.tsx
+++ packages/design-mui/src/components/MuiFilters/components/inputs/DateInput.tsx
@@ -3,9 +3,6 @@ import { TextField } from "@mui/material"
 import type { FilterableControls } from "../../constants.js"
 
 const MILLISECONDS_IN_DAY = 1000 * 60 * 60 * 24
-const MILLISECONDS_IN_WEEK = MILLISECONDS_IN_DAY * 7
-const MILLISECONDS_IN_YEAR = MILLISECONDS_IN_DAY * 365
-const MILLISECONDS_IN_DECADE = MILLISECONDS_IN_YEAR * 10
 
 const DateInput: React.FC<{
   controlType: FilterableControls
@@ -21,13 +18,10 @@ const DateInput: React.FC<{
       variant="standard"
       type={
         step === "day" ||
-        step === MILLISECONDS_IN_DAY ||
         step === "week" ||
-        step === MILLISECONDS_IN_WEEK ||
         step === "year" ||
-        step === MILLISECONDS_IN_YEAR ||
         step === "decade" ||
-        step === MILLISECONDS_IN_DECADE
+        (typeof step === "number" && step % MILLISECONDS_IN_DAY === 0)
           ? "date"
           : "datetime-local"
       }
