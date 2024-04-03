4d5ceda0448a2e97b0edf544962c079b0962568e
roy-coder <52110946+roy-coder@users.noreply.github.com>
2024-04-03 13:14:59 -0400

    Export convertToHatchifyResources (#528)

--- packages/rest-client-jsonapi/src/rest-client-jsonapi.ts
+++ packages/rest-client-jsonapi/src/rest-client-jsonapi.ts
@@ -1,4 +1,10 @@
 import { jsonapi } from "./services/index.js"
 
-export { createOne, findAll, findOne, updateOne } from "./services/index.js"
+export {
+  convertToHatchifyResources,
+  createOne,
+  findAll,
+  findOne,
+  updateOne,
+} from "./services/index.js"
 export default jsonapi
